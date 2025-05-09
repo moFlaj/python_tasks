c8dc5cbee3b10f6206b61df840fc95d47ef352e4
Adebola Folorunsho Ayomide <141844110+moFlaj@users.noreply.github.com>
2025-05-09 19:03:47 +0100

    Add files via upload

---
+++ exponent_value.py
@@ -0,0 +1,20 @@
+
+counter = 1
+sum_factorial = 0
+factorial = 1
+sum_factorial = 0
+
+
+
+for number in range(0,10):
+	for numbers in range(1, counter+1):
+		factorial = numbers * factorial
+	exponent = 1/factorial
+	sum_factorial+=exponent
+	counter+=1	
+
+	print(1 + sum_factorial)
+		
+		
+		
+	
