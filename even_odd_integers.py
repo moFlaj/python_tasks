92396395b330a08e180ea397b8867d57004582f9
Adebola Folorunsho Ayomide <141844110+moFlaj@users.noreply.github.com>
2025-05-10 13:56:52 +0100

    Delete even_odd_integers.py

--- even_odd_integers.py
+++
@@ -1,8 +0,0 @@
-number = int(input('Enter integer to check if it is an even or odd number: '))
-
-if number % 2 == 0:
-	print(number, 'is an even number')
-
-if number % 2 != 0:
-	print(number, 'is an odd number')
-
