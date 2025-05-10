457203712bf46c0193472a7ae530f5a2633954f8
Adebola Folorunsho Ayomide <141844110+moFlaj@users.noreply.github.com>
2025-05-10 14:02:22 +0100

    Delete turing_test.py

--- turing_test.py
+++
@@ -1,6 +0,0 @@
-problem = input('What is your problem? ')
-answer = input('Have you had this problem before?(Yes or No?) ')
-if answer == 'Yes':
-	print('Well, you have it again.')
-elif answer == 'No':
-	print('Well, you have it now.')
